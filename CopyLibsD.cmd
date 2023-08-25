542db53defdafe6723d34301d71bf1a918687940
wongfei <muutant@gmail.com>
2023-08-26 00:42:54 +0300

    copy scripts

---
+++ CopyLibsD.cmd
@@ -0,0 +1,9 @@
+
+copy /Y _cmake\dependencies\submodules\csv-io\RelWithDebInfo\csv-io.lib _lib\
+copy /Y _cmake\dependencies\submodules\delta-studio\engines\basic\RelWithDebInfo\delta-basic.lib _lib\
+copy /Y _cmake\dependencies\submodules\delta-studio\physics\RelWithDebInfo\delta-physics.lib _lib\
+copy /Y _cmake\dependencies\submodules\delta-studio\RelWithDebInfo\delta-core.lib _lib\
+copy /Y _cmake\dependencies\submodules\piranha\RelWithDebInfo\piranha.lib _lib\
+copy /Y _cmake\dependencies\submodules\simple-2d-constraint-solver\RelWithDebInfo\simple-2d-constraint-solver.lib _lib\
+copy /Y _cmake\RelWithDebInfo\engine-sim.lib _lib\
+copy /Y _cmake\RelWithDebInfo\engine-sim-script-interpreter.lib _lib\
